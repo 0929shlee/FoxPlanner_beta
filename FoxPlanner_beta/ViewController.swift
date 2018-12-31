f710db4124809bd466f382e0553dcb9dee73cb8f
Seunghyun Lee <seunghyunlee@seunghyuns-mac.local>
2018-12-31 19:40:40 +0900

    Initial Commit

---
+++ FoxPlanner_beta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoxPlanner_beta
+//
+//  Created by Seunghyun Lee on 31/12/2018.
+//  Copyright © 2018 Seunghyun Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
